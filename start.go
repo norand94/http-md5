25b5e22cb385af179bea31cf71991b6047eff59d
norand94 <norand.developer93@gmail.com>
2017-01-16 23:59:13 +0300

    Теперь приложение отправляет http-status, добавлена функция восстановления при ошибке

--- start.go
+++ start.go
@@ -7,13 +7,29 @@ import (
 )
 
 func connect(w http.ResponseWriter, r *http.Request){
-	//fmt.Println(r)
-	w.Header().Set("Access-Control-Allow-Origin", "*")
+	defer func() {
+		if r := recover(); r != nil {
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		}
+	}()
 
 	request := r.PostFormValue("request")
+	if request == "" {
+		http.Error(w, "Empty Request", http.StatusBadRequest)
+		return
+	}
 	message := core.ReadJSON(request)
+
+	err := core.ValidateMessage(message)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
 	hash := core.GetHash(message)
+	w.WriteHeader(200)
 	fmt.Fprint(w, hash)
+
 }
 
 func main()  {
